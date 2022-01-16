7f717a323cd50dc21310e250507badde56446b8d
Heitor Santos <heitorg33@gmail.com>
2022-01-16 20:19:10 -0300

    Codigo refatorado

--- service/salesService.js
+++ service/salesService.js
@@ -1,10 +1,6 @@
 // const Joi = require('@hapi/joi');
 const { insertSalesModel } = require('../models/salesModel');
 
-// const productsSchema = Joi.object({
-//   quantity: Joi.number().greater(0).required(),
-// });
-
 const danger = (code = 'invalid_data', message, status = 422) => ({
   err: {
     code,
@@ -14,14 +10,11 @@ const danger = (code = 'invalid_data', message, status = 422) => ({
 });
  const insertSalesSerive = async (itensSold) => {
    const quantidade = itensSold.some(({ quantity }) => {
-    //  const { error } = productsSchema.validate({ quantity });
     if (quantity <= 0 || typeof quantity === 'string') { 
       return true;
     }
     return false;
   });  
-
-  // console.log(quantidade);
 
   if (!quantidade) {
     const sales = await insertSalesModel(itensSold);
